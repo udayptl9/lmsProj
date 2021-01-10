4c6954628dd442d0bdc8574c88b05a719aeb48b2
Uday Patil <38643264+udayptl9@users.noreply.github.com>
2021-01-10 19:33:29 +0530

    Update dbcon.php

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','Bagalkot@1','capstone') or die(mysqli_error());
+$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
 ?>
